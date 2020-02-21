007a3783b7b638d83dcfb73de7811af3a430e67f
Gyan Mohanty <gyanchand.mohanty@calsofthq.com>
2020-02-21 13:10:09 +0530

    map baics reloded

--- 06_map/01_basic/main.go
+++ 06_map/01_basic/main.go
@@ -1,24 +1,43 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	var m2 map[string]string
-	m1 := make(map[int]string)
-	fmt.Println(m1)
-	m1[1] = "one"
-	m1[2] = "two"
-	fmt.Println(m1)
-	m1[3] = "three"
-
-	fmt.Println(m2)
-	fmt.Println(m2 == nil)
-	m2["gg"] = "dd"
-
-	for i, v := range m1 {
-		fmt.Println(i, v)
+	//Declaring a map
+	var m map[string]int
+	if m == nil {
+		fmt.Println("m is nil", m)
 	}
+	// Attempting to add keys to a nil map will result in a runtime error
+	// m["one hundred"] = 100
+
+	// Initializing a map using the built-in make() function
+	var m2 = make(map[string]int)
+	fmt.Println(m2)
+	if m2 == nil {
+		fmt.Println("m2 is nil")
+	} else {
+		fmt.Println("m2 is not nil")
+	}
+	// make() function returns an initialized and ready to use map.
+	// Since it is initialized, you can add new keys to it.
+	m2["hundred"] = 100
+	fmt.Println(m2)
+
+	// Initializing a map using a map literal
+	var m3 = map[string]int{
+		"one": 1,
+		"two": 2,
+	}
+	fmt.Println(m3)
+	// create an empty map using a map literal by leaving the curly braces empty
+	var m4 = map[string]int{}
+	m4 = m3
+	fmt.Println(m4)
+
+	// Adding items (key-value pairs) to a map
+	m4["three"] = 3
+	m4["four"] = 4
+	fmt.Println(m4)
 
 }
