e19b0f4550c5da9df616a8d7b7f18262a3b911e8
Gyan Mohanty <gyanchand.mohanty@calsofthq.com>
2020-03-03 15:02:59 +0530

    all about one and only for loop

---
+++ 21_control/03_loop/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	var sum int
+	for i := 0; i <= 15; i++ {
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("Afer Continue")
+		sum += i
+		if sum > 30 {
+			break
+		}
+		fmt.Println("Afer break")
+		fmt.Println(sum)
+	}
+	fmt.Println(sum)
+
+	for x := 0; x <= 5; x++ {
+		for y := 0; y < 5; y++ {
+			if x == 2 && y == 3 { //skip printing "inner loop when x is 2"
+				continue
+			}
+			fmt.Println("inner loop X:", x, " Y:", y)
+			sum = x + y
+		}
+		if x == 3 {
+			break
+		}
+		fmt.Println("outer loop X: ", x)
+	}
+}
---
+++ 21_control/03_loop/readme.md
@@ -0,0 +1,3 @@
+break will break the loop, if nested loop is there, break will only break the loop where it called.
+
+Continue will continue the flow of execution in the loop excluding below lines of code.
